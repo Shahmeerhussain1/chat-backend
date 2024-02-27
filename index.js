3945b3fba08086141563b4f2ac536b95edbd6817
Shah Meer <hussainshahmeer99@gmail.com>
2024-02-27 07:51:20 -0800

    changes

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const express = require('express')
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const User = require('./Schemas/userSchema')
 const mongoose = require('mongoose')
+const cors = require('cors')
 const app = express()
-const port = 3000
+const corsOptions = {
+  origin: 'http://localhost:3000', 
+  optionsSuccessStatus: 200 
+};
+app.use(cors(corsOptions))
+const port = 8080
 
 
 
 
 
-
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -18,7 +25,7 @@ app.get('/', (req, res) => {
 app.get('/allUser', async (req, res) => {
   if (req.query._id) {
     const user = await User.find();
-    console.log('users', user)
+    // console.log('users', user)
     let flag = false
     for (const iterator of user) {
       if (iterator._id == req.query._id) {
@@ -27,37 +34,23 @@ app.get('/allUser', async (req, res) => {
       }
     }
     if(flag){
-
       res.status(200).json({ success: true, data: user.filter((ele)=>{return ele._id !== req.query._id}) })
     }else{
       res.status(200).json({ success: false, data: '_id not found' })
-
     }
-
-
   } else {
     res.status(500).json({ message: "id not provided", success: false });
   }
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+
 
 
 
 //WMfNIIRJCqzncEHM
-
-
 // const uri = "mongodb+srv://hussainshahmeer99:WMfNIIRJCqzncEHM@cluster0.0fa2fsk.mongodb.net/Chats?retryWrites=true&w=majority&appName=Cluster0";
 const uri = "mongodb+srv://hussainshahmeer99:WMfNIIRJCqzncEHM@cluster0.0fa2fsk.mongodb.net/Chats"
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   }
-// });
+
 
 async function run() {
   try {
